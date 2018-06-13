f4e91fdeabe211384ae98dc32d0128574a0b2598
Nikita Livyk <livik.nick@gmail.com>
2018-06-13 22:48:06 +0300

    Исправил импорт шрифтоф в билд. Подключил шрифты, наверстал мобильное меню

--- gulpfile.js
+++ gulpfile.js
@@ -48,9 +48,9 @@ gulp.task("images", function() {
 
 gulp.task('copy', function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/*.{woff,woff2}",
     "source/*.html"
-  ])
+  ], {base: 'source'})
   .pipe(gulp.dest("build"));
 });
 
