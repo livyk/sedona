f896bf63bb62c92f06fef724b765a5ff39be5ddd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-02 20:06:58 +0000

    Deploy av1at0r/sedona to github.com/av1at0r/sedona.git:gh-pages

--- js/index.js
+++ js/index.js
@@ -2,19 +2,27 @@ const nav = document.querySelector("nav.main-nav"),
   openNavButton = document.querySelector(".main-nav__burger-button"),
   closeNavButton = document.querySelector(".main-nav__close-button");
 
+
 openNavButton.onclick = function () {
   if (nav.classList.contains("main-nav--closed")) {
     nav.classList.remove("main-nav--closed");
-    nav.classList.add("main-nav--opened");
+    nav.classList.add("main-nav--showing");
+    document.querySelector('.main-nav__list-item > a[href]').focus();
   } else {
-    nav.classList.remove("main-nav--opened");
-    nav.classList.add("main-nav--closed");
+    nav.classList.add("main-nav--hidding");
   }
 };
 
-closeNavButton.onclick = function () {
-  if (nav.classList.contains("main-nav--opened")) {
-    nav.classList.remove("main-nav--opened");
+document.querySelector('.main-nav')
+.addEventListener("animationend", function() {
+  if(nav.classList.contains("main-nav--hidding")) {
+    nav.classList.remove("main-nav--hidding");
     nav.classList.add("main-nav--closed");
+  } else {
+    nav.classList.remove("main-nav--showing");
   }
+}, false);
+
+closeNavButton.onclick = function () {
+  nav.classList.add("main-nav--hidding");
 };
